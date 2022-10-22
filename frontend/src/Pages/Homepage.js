7bf285a0d89bedcaac7d86469cdeec9559d1ee90
8ahmedanwer8 <sahmedanwer@gmail.com>
2022-10-22 13:33:28 -0400

    done homepage

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -3,6 +3,7 @@ import {
   Button,
   Container,
   Drawer,
+  Center,
   DrawerBody,
   DrawerFooter,
   DrawerHeader,
@@ -25,8 +26,58 @@ const HomePage = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const btnRef = React.useRef();
   return (
-    <Container backgroundColor="#F9F4F5" p="0" h="fit-content" maxW="100%">
-      <Heading>page that asks if ur an airlines or a warehouse</Heading>
+    <Container backgroundColor="#2A2C36" p="0" h="fit-content" maxW="100%">
+      <Box h="100vh">
+        <Center h="80%">
+          <Box w="fit-content" h="fit-content" textAlign="center">
+            <Heading fontFamily="Roboto Flex" color="#FFF" fontSize="96">
+              WarePal
+            </Heading>
+            <Heading
+              fontFamily="Poppins"
+              color="#FFF"
+              fontWeight="300"
+              fontSize="48"
+            >
+              Own your warehouse
+            </Heading>
+            <Button
+              fontFamily="Inter"
+              color="#FFF"
+              borderRadius="115"
+              padding="12"
+              backgroundColor="#D0823E"
+              m="8"
+              fontSize="24"
+              _hover={{ opacity: "0.5" }}
+              _active={{
+                bg: "#C46C20",
+                transform: "scale(0.97)",
+                borderColor: "#bec3c9",
+              }}
+            >
+              We are a Warehouse
+            </Button>
+            <Button
+              fontFamily="Inter"
+              borderRadius="115"
+              color="#FFF"
+              fontSize="24"
+              padding="12"
+              backgroundColor="#D0823E"
+              _hover={{ opacity: "0.5" }}
+              _active={{
+                bg: "#C46C20",
+                transform: "scale(0.97)",
+                borderColor: "#bec3c9",
+              }}
+              m="8"
+            >
+              We are an Airlines
+            </Button>
+          </Box>
+        </Center>
+      </Box>
     </Container>
   );
 };
